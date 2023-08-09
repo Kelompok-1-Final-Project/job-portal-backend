e17dddeab2e742256145acf7b1a626e37645997b
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-09 14:02:53 +0700

    Adding Dao Admin

---
+++ admin/src/main/java/com/lawencon/jobportal/admin/dao/MaritalStatusDao.java
@@ -0,0 +1,10 @@
+package com.lawencon.jobportal.admin.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.lawencon.base.AbstractJpaDao;
+
+@Repository
+public class MaritalStatusDao extends AbstractJpaDao{
+
+}
