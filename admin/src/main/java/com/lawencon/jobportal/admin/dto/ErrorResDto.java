8a3b7cd4ae22e72e053ee7f93b2060373c80aa51
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-10 09:38:55 +0700

    Adding DTO General

---
+++ admin/src/main/java/com/lawencon/jobportal/admin/dto/ErrorResDto.java
@@ -0,0 +1,13 @@
+package com.lawencon.jobportal.admin.dto;
+
+public class ErrorResDto<T> {
+	private T message;
+
+	public T getMessage() {
+		return message;
+	}
+
+	public void setMessage(T message) {
+		this.message = message;
+	}
+}
