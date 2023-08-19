ffaf57d2e4f4a3e490c1a0157358f9ba33209bab
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-19 16:51:29 +0700

    Revise Skill Test

--- candidate/src/main/java/com/lawencon/jobportal/candidate/controller/UserController.java
+++ candidate/src/main/java/com/lawencon/jobportal/candidate/controller/UserController.java
@@ -3,12 +3,15 @@ package com.lawencon.jobportal.candidate.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.jobportal.candidate.dto.InsertResDto;
+import com.lawencon.jobportal.candidate.dto.user.UserGetResDto;
 import com.lawencon.jobportal.candidate.dto.user.UserInsertReqDto;
 import com.lawencon.jobportal.candidate.dto.user.UserRegisterByAdminReqDto;
 import com.lawencon.jobportal.candidate.service.UserService;
@@ -30,5 +33,11 @@ public class UserController {
 	public ResponseEntity<InsertResDto> registerByAdmin(@RequestBody UserRegisterByAdminReqDto data){
 		final InsertResDto response = userService.insertCandidate(data);
 		return new ResponseEntity<>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<UserGetResDto> getByCandidate(@PathVariable String candidateId){
+		final UserGetResDto response = userService.getByCandidate(candidateId);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
---
+++ candidate/src/main/java/com/lawencon/jobportal/candidate/dto/user/UserGetResDto.java
@@ -0,0 +1,113 @@
+package com.lawencon.jobportal.candidate.dto.user;
+
+public class UserGetResDto {
+	private String fullName;
+	private String idNumber;
+	private String phone;
+	private String email;
+	private String maritalStatusId;
+	private String maritalStatus;
+	private String gender;
+	private Integer expectedSalary;
+	private String age;
+	private String cvId;
+	private String photoId;
+	private String summary;
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMaritalStatusId() {
+		return maritalStatusId;
+	}
+
+	public void setMaritalStatusId(String maritalStatusId) {
+		this.maritalStatusId = maritalStatusId;
+	}
+
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Integer getExpectedSalary() {
+		return expectedSalary;
+	}
+
+	public void setExpectedSalary(Integer expectedSalary) {
+		this.expectedSalary = expectedSalary;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getCvId() {
+		return cvId;
+	}
+
+	public void setCvId(String cvId) {
+		this.cvId = cvId;
+	}
+
+	public String getPhotoId() {
+		return photoId;
+	}
+
+	public void setPhotoId(String photoId) {
+		this.photoId = photoId;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+}
