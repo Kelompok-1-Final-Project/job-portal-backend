c3f316f40b7ecf601d7e258d6cc432330e6606ce
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-21 22:27:45 +0700

    Revise Result

--- admin/src/main/java/com/lawencon/jobportal/admin/controller/ResultController.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/controller/ResultController.java
@@ -1,9 +1,13 @@
 package com.lawencon.jobportal.admin.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lawencon.jobportal.admin.dto.DeleteResDto;
 import com.lawencon.jobportal.admin.dto.InsertResDto;
+import com.lawencon.jobportal.admin.dto.result.ResultGetResDto;
 import com.lawencon.jobportal.admin.dto.result.ResultInsertReqDto;
 import com.lawencon.jobportal.admin.service.ResultService;
 
@@ -39,5 +44,11 @@ public class ResultController {
 			response.setMessage("Delete Result failed.");
 			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 		}
+	}
+	
+	@GetMapping("/job/{jobId}")
+	public ResponseEntity<List<ResultGetResDto>> getByJob(@PathVariable String jobId){
+		final List<ResultGetResDto> response = resultService.getAllByJob(jobId);
+		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 }
