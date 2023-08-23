2445df7e2e0b9205ce0bfa55304bd3d992f83cfa
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-23 09:14:28 +0700

    Revise City

--- candidate/src/main/java/com/lawencon/jobportal/candidate/service/CityService.java
+++ candidate/src/main/java/com/lawencon/jobportal/candidate/service/CityService.java
@@ -6,15 +6,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.base.ConnHandler;
-import com.lawencon.config.JwtConfig;
 import com.lawencon.jobportal.candidate.dao.CityDao;
 import com.lawencon.jobportal.candidate.dto.InsertResDto;
 import com.lawencon.jobportal.candidate.dto.UpdateResDto;
@@ -48,7 +42,7 @@ public class CityService {
 
 			result.setId(cities.getId());
 			result.setMessage("City added successfully.");
-			
+
 		} catch (Exception e) {
 			em().getTransaction().rollback();
 			e.printStackTrace();
@@ -69,41 +63,25 @@ public class CityService {
 		}
 		return listResult;
 	}
-	
+
 	public UpdateResDto updateCity(CityUpdateReqDto data) {
 		final UpdateResDto result = new UpdateResDto();
 		try {
 			em().getTransaction().begin();
-			
+
 			final City cityDb = cityDao.getByCode(data.getCityCode());
-			final City city = cityDao.getById(City.class,cityDb.getId());
+			final City city = cityDao.getById(City.class, cityDb.getId());
 			city.setCityName(data.getCityName());
 			final City cityResult = cityDao.save(city);
-			
-			final String updateCityCandidateAPI = "http://localhost:8081/cities";
 
-			final HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_JSON);
-			headers.setBearerAuth(JwtConfig.get());
-			
-			final RequestEntity<CityUpdateReqDto> cityUpdate = RequestEntity.patch(updateCityCandidateAPI).headers(headers)
-					.body(data);
+			result.setVersion(cityResult.getVersion());
+			result.setMessage("City updated successfully.");
+			em().getTransaction().commit();
 
-			final ResponseEntity<UpdateResDto> responseCandidate = restTemplate.exchange(cityUpdate, UpdateResDto.class);
-
-			if (responseCandidate.getStatusCode().equals(HttpStatus.OK)) {
-				result.setVersion(cityResult.getVersion());
-				result.setMessage("City updated successfully.");
-				em().getTransaction().commit();
-			} else {
-				em().getTransaction().rollback();
-				throw new RuntimeException("Update Failed");
-			}
-			
 		} catch (Exception e) {
 			em().getTransaction().rollback();
 		}
-		
+
 		return result;
 	}
 
