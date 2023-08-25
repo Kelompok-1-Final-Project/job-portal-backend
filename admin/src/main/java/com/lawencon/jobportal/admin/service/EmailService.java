677e3a8e0483ad6911a29d1ac07c1ae16c248833
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-25 09:26:52 +0700

    Add Email Thread

--- admin/src/main/java/com/lawencon/jobportal/admin/service/EmailService.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/service/EmailService.java
@@ -5,31 +5,24 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import com.lawencon.jobportal.admin.dto.InsertResDto;
-
 @Service
 public class EmailService {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
 
-	public InsertResDto sendEmail(String to, String subject, String body){
-		System.out.println("Sending Email...");
+	public void sendEmail(String to, String subject, String body){
 		
-		final SimpleMailMessage msg = new SimpleMailMessage();
-		msg.setTo(to);
-
-		msg.setSubject(subject);
-		msg.setText(body);
-
-		javaMailSender.send(msg);
-
-		System.out.println("Done");
-
-		final InsertResDto insertResDto = new InsertResDto();
-
-		insertResDto.setMessage("Kirim Email Berhasil");
-
-		return insertResDto;
+		Thread thread = new Thread(){
+            public void run(){
+                SimpleMailMessage msg = new SimpleMailMessage();
+				msg.setTo(to);
+				msg.setSubject(subject);
+				msg.setText(body);
+				javaMailSender.send(msg);
+            }
+        };
+        
+        thread.start();
 	}
 }
