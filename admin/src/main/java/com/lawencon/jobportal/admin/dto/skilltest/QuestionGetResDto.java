4bddeff90990dd152ac99746bf9b14c21c26a465
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-25 09:40:56 +0700

    Add Question Code to Skill Test GEt

--- admin/src/main/java/com/lawencon/jobportal/admin/dto/skilltest/QuestionGetResDto.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/dto/skilltest/QuestionGetResDto.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class QuestionGetResDto {
 	private String question;
 	private String questionId;
+	private String questionCode;
 	private List<QuestionOptionGetResDto> optionGetResDtos;
 
 	public String getQuestion() {
@@ -21,6 +22,14 @@ public class QuestionGetResDto {
 
 	public void setQuestionId(String questionId) {
 		this.questionId = questionId;
+	}
+
+	public String getQuestionCode() {
+		return questionCode;
+	}
+
+	public void setQuestionCode(String questionCode) {
+		this.questionCode = questionCode;
 	}
 
 	public List<QuestionOptionGetResDto> getOptionGetResDtos() {
