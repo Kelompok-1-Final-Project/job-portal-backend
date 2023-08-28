d15a4603db9e7b2e949830a78d081b6309d1d939
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-28 11:06:30 +0700

    Revise Assessment

--- admin/src/main/java/com/lawencon/jobportal/admin/dao/AssessmentDao.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/dao/AssessmentDao.java
@@ -11,10 +11,14 @@ import org.springframework.stereotype.Repository;
 import com.lawencon.base.AbstractJpaDao;
 import com.lawencon.base.ConnHandler;
 import com.lawencon.jobportal.admin.model.Assessment;
+import com.lawencon.jobportal.admin.model.Candidate;
+import com.lawencon.jobportal.admin.model.CandidateProfile;
 import com.lawencon.jobportal.admin.model.Company;
 import com.lawencon.jobportal.admin.model.File;
 import com.lawencon.jobportal.admin.model.Job;
 import com.lawencon.jobportal.admin.model.JobStatus;
+import com.lawencon.jobportal.admin.model.Profile;
+import com.lawencon.jobportal.admin.model.User;
 
 @Repository
 public class AssessmentDao extends AbstractJpaDao {
@@ -22,6 +26,59 @@ public class AssessmentDao extends AbstractJpaDao {
 		return ConnHandler.getManager();
 	}
 
+	public List<Assessment> getByUser(String userId) {
+		final StringBuilder sql = new StringBuilder();
+		sql.append("SELECT ta.id AS assessment_id, tj.id AS job_id, tj.job_title, ta.hr_id , tp.full_name,  ");
+		sql.append("ta.schedule, ta.notes, tc.id AS candidate_id, tcp.full_name AS candidate_name  ");
+		sql.append("FROM t_assessment ta ");
+		sql.append("INNER JOIN t_job tj ON ta.job_id = tj.id ");
+		sql.append("INNER JOIN t_candidate tc ON ta.candidate_id = tc.id  ");
+		sql.append("INNER JOIN t_candidate_profile tcp ON tc.profile_id = tcp.id ");
+		sql.append("INNER JOIN t_user tu ON ta.hr_id = tu.id ");
+		sql.append("INNER JOIN t_profile tp ON tu.profile_id = tp.id ");
+		sql.append("WHERE tj.hr_id = :userId OR tj.interviewer_id = :userId ");
+
+		final List<?> assessmentObjs = this.em().createNativeQuery(sql.toString())
+				.setParameter("userId", userId)
+				.getResultList();
+		
+		final List<Assessment> listAssessments = new ArrayList<>();
+		if (assessmentObjs.size() > 0) {
+			for (Object assessmentObj : assessmentObjs) {
+				final Object[] assessmentArr = (Object[]) assessmentObj;
+
+				final Assessment assessment = new Assessment();
+				assessment.setId(assessmentArr[0].toString());
+
+				final Job job = new Job();
+				job.setId(assessmentArr[1].toString());
+				job.setJobTitle(assessmentArr[2].toString());
+		
+				final User user = new User();
+				user.setId(assessmentArr[3].toString());
+				
+				final Profile profile = new Profile();
+				profile.setFullName(assessmentArr[4].toString());
+				user.setProfile(profile);
+				
+				assessment.setSchedule(Timestamp.valueOf(assessmentArr[5].toString()).toLocalDateTime());
+				assessment.setNotes(assessmentArr[6].toString());
+				
+				final Candidate candidate = new Candidate();
+				candidate.setId(assessmentArr[7].toString());
+				
+				final CandidateProfile candidateProfile = new CandidateProfile();
+				candidateProfile.setFullName(assessmentArr[8].toString());
+				candidate.setCandidateProfile(candidateProfile);
+				assessment.setCandidate(candidate);
+				
+				listAssessments.add(assessment);
+			}
+		}
+
+		return listAssessments;
+	}
+	
 	public List<Assessment> getByCandidate(String candidateId) {
 		final StringBuilder sql = new StringBuilder();
 		sql.append("SELECT ta.id AS assessment_id, tj.id AS job_id, tj.job_title, tjs.status_name, ");
