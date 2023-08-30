1594f72d03769fbb292f65b7a36011e30588a1b3
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-08-30 17:44:55 +0700

    Revise Company

--- candidate/src/main/java/com/lawencon/jobportal/candidate/dto/company/CompanyGetResDto.java
+++ candidate/src/main/java/com/lawencon/jobportal/candidate/dto/company/CompanyGetResDto.java
@@ -10,6 +10,8 @@ public class CompanyGetResDto {
 	private String description;
 	private String address;
 	private String cityName;
+	private String createdAt;
+	private String updatedAt;
 
 	public String getCityName() {
 		return cityName;
@@ -73,6 +75,22 @@ public class CompanyGetResDto {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public String getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
 	}
 
 }
