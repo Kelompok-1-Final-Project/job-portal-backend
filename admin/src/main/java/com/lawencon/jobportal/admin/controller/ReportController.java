1e5f80a7d8bccb5073c2f6392ba798a41b60d909
Muhammad Anggi Wirahmat <58240412+wirahmat@users.noreply.github.com>
2023-09-01 16:03:59 +0700

    Revise Report

--- admin/src/main/java/com/lawencon/jobportal/admin/controller/ReportController.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/controller/ReportController.java
@@ -3,6 +3,7 @@ package com.lawencon.jobportal.admin.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,20 @@ public class ReportController {
 	@Autowired
 	private ReportService reportService;
 	
-	@GetMapping()
+	@GetMapping
 	public ResponseEntity<List<ReportGetResDto>> getReport(@RequestParam("start") String startDate, 
 			@RequestParam("end") String endDate) {
 		final List<ReportGetResDto> response = reportService.getReport(startDate, endDate);
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
+	
+	@GetMapping("/download")	
+    public ResponseEntity<?> downloadReport(@RequestParam("start") String startDate, @RequestParam("end") String endDate) {
+        final String fileName = "Application Report";
+        final byte[] reportByteArr = reportService.downloadReport(startDate, endDate);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName + ".pdf")
+                .body(reportByteArr);
+   }
 	
 }
--- admin/src/main/java/com/lawencon/jobportal/admin/service/ReportService.java
+++ admin/src/main/java/com/lawencon/jobportal/admin/service/ReportService.java
@@ -1,13 +1,19 @@
 package com.lawencon.jobportal.admin.service;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lawencon.jobportal.admin.dao.ReportDao;
 import com.lawencon.jobportal.admin.dto.report.ReportGetResDto;
+import com.lawencon.jobportal.admin.dto.report.ReportJasperGetReqDto;
+import com.lawencon.util.JasperUtil;
 
 @Service
 public class ReportService {
@@ -15,9 +21,21 @@ public class ReportService {
 	@Autowired
 	private ReportDao reportDao;
 	
+	@Autowired
+	private JasperUtil jasperUtil;
+	
 	public List<ReportGetResDto> getReport(String startDate, String endDate) {
 		final List<ReportGetResDto> reportGetResDtos = new ArrayList<>();
-		reportDao.getReport(startDate, endDate).forEach(r -> {
+		LocalDate startDateLocal = null;
+		LocalDate endDateLocal = null;
+		if(startDate != null && !startDate.equals("undefined")) {
+			startDateLocal = Date.valueOf(startDate).toLocalDate();	
+		}
+		if(endDate != null && !endDate.equals("undefined")){
+			endDateLocal = Date.valueOf(endDate).toLocalDate();		
+		}
+		
+		reportDao.getReport(startDateLocal, endDateLocal).forEach(r -> {
 			final ReportGetResDto reportGetResDto = new ReportGetResDto();
 			reportGetResDto.setCandidateName(r.getCandidateName());
 			reportGetResDto.setJobName(r.getJobName());
@@ -27,5 +45,46 @@ public class ReportService {
 		});
 		
 		return reportGetResDtos;
+	}
+	
+	public byte[] downloadReport(String startDate, String endDate) {
+		try {
+			final Map<String, Object> reportData = new HashMap<>();
+			
+			final LocalDate now = LocalDate.now();
+			
+			reportData.put("today", now.toString());
+			reportData.put("startDate", startDate);
+			reportData.put("endDate", endDate);
+
+			final List<ReportJasperGetReqDto> reportJasperGetReqDtos = new ArrayList<>();
+			
+			LocalDate startDateLocal = null;
+			LocalDate endDateLocal = null;
+			if(startDate != null && !startDate.equals("undefined")) {
+				startDateLocal = Date.valueOf(startDate).toLocalDate();	
+			}
+			if(endDate != null && !endDate.equals("undefined")){
+				endDateLocal = Date.valueOf(endDate).toLocalDate();		
+			}
+			
+			reportDao.getReport(startDateLocal, endDateLocal).forEach(r -> {
+				final ReportJasperGetReqDto reportJasperGetReqDto = new ReportJasperGetReqDto();
+				reportJasperGetReqDto.setFullName(r.getCandidateName());
+				reportJasperGetReqDto.setJobTitle(r.getJobName());
+				reportJasperGetReqDto.setEmploymentName(r.getEmploymentTypeName());
+				reportJasperGetReqDto.setDateDiff(r.getDateDiff());
+				reportJasperGetReqDtos.add(reportJasperGetReqDto);
+			});
+			
+			
+			final byte[] reportByteArr = jasperUtil.responseToByteArray(reportJasperGetReqDtos, reportData, "reportHired");
+			
+			return reportByteArr;
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
 	}
 }
